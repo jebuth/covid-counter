13cda36e7dc4946b966606f02517b545aafb5278
justin <justinbuth91@live.com>
2020-05-09 18:13:21 -0700

    done

--- src/App.js
+++ src/App.js
@@ -4,27 +4,40 @@ import {Cards, Chart, CountryPicker} from './components';
 import styles from './App.module.css';
 import { fetchData } from './api';
 
+import headerImage from './images/covid-header-image.png';
+
 class App extends React.Component{
     
     state = {
         data: {},
+        country: '',
     }
 
     async componentDidMount(){        
-         const data = await fetchData();
+         const data = await fetchData(); // with null argument
 
          this.setState({ data });
     }
 
-    render(){
+    handleCountryChange = async (country) => {
 
-        const {data} = this.state;
+        // fetch data
+        const data = await fetchData(country); // with country as argument
+
+        //console.log(data);
+        // set state
+        this.setState({data, country});
+    }
+
+    render(){
+        const {data, country} = this.state;
 
         return(
             <div className = { styles.container }>
+                <img className={styles.image} src={headerImage} alt="COVID-19"/>
                 <Cards data={ data }/>
-                <CountryPicker />
-                <Chart />
+                <CountryPicker handleCountryChange={this.handleCountryChange} />
+                <Chart data={data} country={country}/>
             </div>
         )
     }
--- src/api/index.js
+++ src/api/index.js
@@ -2,10 +2,16 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
     try{
+
+        let dynamicUrl = url;
+        if(country){
+            dynamicUrl = `${url}/countries/${country}`;
+        }
+
         // destructure data from the response
-        const {data: {confirmed, recovered, deaths, lastUpdate}} = await axios.get(url);
+        const {data: {confirmed, recovered, deaths, lastUpdate}} = await axios.get(dynamicUrl);
         
         const modifiedData = {
             confirmed,
@@ -16,7 +22,7 @@ export const fetchData = async () => {
         
         return modifiedData;
     } catch(error){
-
+        console.log(error)
     }
 }
 
@@ -33,6 +39,17 @@ export const fetchDailyData = async () => {
         return modifiedData;
     }
     catch(error){
+        console.log(error)
+    }
+}
 
+export const fetchCountries = async () => {
+    try {
+        const {data: {countries}} = await axios.get(`${url}/countries`);
+
+        return countries.map((country) => country.name);
+    }
+    catch(error){
+        console.log(error);
     }
 }
